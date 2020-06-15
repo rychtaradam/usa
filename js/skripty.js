b9cbbd4e2ef1acbbdfa3abe6687c0c67e004df4b
rychtaradam <46920772+rychtaradam@users.noreply.github.com>
2020-06-15 20:13:01 +0200

    Add files via upload

--- js/skripty.js
+++ js/skripty.js
@@ -7,35 +7,63 @@ $(document).ready(function(){
 });
 
 // create the map
-var mymap = L.map('mapid', {
+let mymap = L.map('mapid', {
     center: [-0.0, -0.0],
     zoom: 4
 });
 
+let greenIcon = new L.Icon({
+  iconUrl: 'https://cdn.rawgit.com/pointhi/leaflet-color-markers/master/img/marker-icon-2x-green.png',
+  shadowUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/0.7.7/images/marker-shadow.png',
+  iconSize: [25, 41],
+  iconAnchor: [12, 41],
+  popupAnchor: [1, -34],
+  shadowSize: [41, 41]
+});
+
 // create the image
-var imageUrl = './svg/mapa.svg',
+let imageUrl = './svg/mapa.svg',
     imageBounds = [[-27.098, -45.0], [27.098, 45.0]];
 
 L.imageOverlay(imageUrl, imageBounds).addTo(mymap);
 
 //L.rectangle(imageBounds).addTo(mymap);
 
+let mesta = L.layerGroup([]).addTo(mymap);
+let unesco = L.layerGroup([]).addTo(mymap);
+
 $(document).ready(function(){
-    $.getJSON('./data.json', function( data ) {
+    $.getJSON('./json/mesta.json', function( data ) {
         $.each(data, function (i, item) {
-            L.marker([data[i].lat, data[i].lon]).addTo(mymap).bindTooltip("<b>" + data[i].mesto + "</b>, " + data[i].stat).on("click", function(event) {
+            L.marker([data[i].lat, data[i].lon]).addTo(mymap).addTo(mesta).bindTooltip("<b>" + data[i].mesto + "</b>, " + data[i].stat).on("click", function(event) {
                 $('#infobox').collapse("show");
+
                 $('#mesto').text(data[i].mesto);
                 $('#stat').text(data[i].stat);
                 $('#popisek').text(data[i].popisek);
-                $('#wikimesta').attr("href", data[i].wikimesta);
-                $('#wikistatu').attr("href", data[i].wikistatu);
+                $('#wiki').attr("href", data[i].wikimesta);
             })
         });
     });
 });
 
-// mymap.on("contextmenu", function (event) {
-//   alert("Coordinates: " + event.latlng.toString());
-//   L.marker(event.latlng).addTo(mymap);
-// });
+$(document).ready(function(){
+    $.getJSON('./json/unesco.json', function( data ) {
+        $.each(data, function (i, item) {
+            L.marker([data[i].lat, data[i].lon], {icon: greenIcon}).addTo(mymap).addTo(unesco).bindTooltip("<b>" + data[i].pamatka + "</b>, " + data[i].stat).on("click", function(event) {
+                $('#infobox').collapse("show");
+                $('#mesto').text(data[i].pamatka);
+                $('#stat').text(data[i].stat);
+                $('#popisek').text(data[i].popisek);
+                $('#wiki').attr("href", data[i].wiki);
+            })
+        });
+    });
+});
+
+let vrstvy = {
+    "Města": mesta,
+    "Unesco památky": unesco
+};
+
+L.control.layers(null, vrstvy).addTo(mymap);
